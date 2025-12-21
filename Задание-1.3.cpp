a37c36ad3dca2ff85a54ae64c429c7fd42f2e2ed
ezHeaven <nguen.kh@yandex.ru>
2025-12-21 13:12:05 +0300

    Update Задание 1.3

---
+++ Задание-1.3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief вычисляет количество выделенной теплоты
+ * @param c - значение теплоёмкости стали
+ * @param m - значение массы
+ * @param t_0 - значение начальной температуры
+ * @param t - значение конечной температуры
+ */
+float calculateQ(const float c, float m, float t_0, float t);
+
+
+/**
+ * @brief точка входа в программу
+ * @return - 0, если программа корректно выполнена
+ */
+int main() {
+    // ввод константы
+    const float c = 460;
+
+
+    // ввод данных
+    float m, t_0, t;
+    cin >> m >> t_0 >> t;
+    cout << "Введите исходные данные: " << m << " " << t_0 << " " << t << endl;
+    
+    
+    // вычисление количества израсходованной теплоты по формуле Q = c * m * (t - t_0)
+    float Q = calculateQ(c, m, t_0, t);
+
+
+    // результ вычислений
+    cout << "Количество израсходованной теплоты: " << Q;
+}
+
+
+float calculateQ(const float c, float m, float t_0, float t) {
+    return c * m * (t - t_0);
+}
